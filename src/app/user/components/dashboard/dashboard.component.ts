d164a5091c5042b70ecf18094916fa174bddf4b5
BAAAWB <92890853+IhebBaoueb@users.noreply.github.com>
2023-09-13 17:43:11 +0100

    front

--- src/app/user/components/dashboard/dashboard.component.ts
+++ src/app/user/components/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 
 import { Component, OnInit } from '@angular/core';
 import { QuestionService } from '../../user-services/question-service/question.service';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,13 +11,29 @@ import { QuestionService } from '../../user-services/question-service/question.s
 })
 export class DashboardComponent implements OnInit {
 
+  questionId: number = this.activatedRoute.snapshot.params["questionId"];
   questions: any[] = [] ;
   pageNum:number =0 ;
   total !: number ;
-  constructor(private service : QuestionService) {}
+  question : any ;
+  searchForm!: FormGroup;
+
+
+  constructor(
+    private service : QuestionService ,
+    private questionService:QuestionService,
+    private activatedRoute: ActivatedRoute,
+    private fb : FormBuilder
+
+
+    ) {}
 
   ngOnInit(){
     this.getAllQuestions();
+    this.searchForm = this.fb.group({
+      questionId: ['', Validators.required], // Assurez-vous de définir des validateurs appropriés.
+    });
+
   }
 
   getAllQuestions() {
@@ -30,5 +48,20 @@ export class DashboardComponent implements OnInit {
     this.pageNum = event.pageIndex;
     this.getAllQuestions();
   }
+
+  onSubmit() {
+    if (this.searchForm.valid) {
+      const questionId = this.searchForm.value.questionId;
+  
+      // Appelez le service pour obtenir la question par ID.
+      this.questionService.getQuestionById(questionId).subscribe((response) => {
+        this.question = response; // Stockez la question dans une variable de votre composant.
+        // Faites ce que vous souhaitez avec la question, par exemple, affichez-la dans un autre emplacement de votre template.
+      });
+    }
+  }
+  
+  
+  
 
 }
